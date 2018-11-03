bd596bbe39c104534974b00c074d3e2a4a35835b
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2018-11-03 18:16:22 +0200

    JSONArray: Add class dummy.

---
+++ project/json-parser/src/main/java/fi/tamk/tiko/ohjelmointi/json/JSONArray.java
@@ -0,0 +1,18 @@
+package fi.tamk.tiko.ohjelmointi.json;
+
+/**
+ *
+ *
+ * @author  Joonas Lauhala {@literal <joonas.lauhala@cs.tamk.fi>}
+ * @version 2018.1101
+ * @since   11
+ */
+public class JSONArray {
+
+    /**
+     *
+     */
+    public JSONArray() {
+
+    }
+}
