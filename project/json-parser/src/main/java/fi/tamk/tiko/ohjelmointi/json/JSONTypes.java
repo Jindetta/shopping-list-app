48af7e10762dd261aae2a70ceba1245321bfddd5
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2018-11-03 20:11:10 +0200

    JSONTypes: Add enum class for types.

---
+++ project/json-parser/src/main/java/fi/tamk/tiko/ohjelmointi/json/JSONTypes.java
@@ -0,0 +1,12 @@
+package fi.tamk.tiko.ohjelmointi.json;
+
+/**
+ *
+ *
+ * @author  Joonas Lauhala {@literal <joonas.lauhala@cs.tamk.fi>}
+ * @version 2018.1101
+ * @since   11
+ */
+public enum JSONTypes {
+    BOOLEAN, NUMBER, DECIMAL, STRING, OBJECT, ARRAY, NULL
+}
