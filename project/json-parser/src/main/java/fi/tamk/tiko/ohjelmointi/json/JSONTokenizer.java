34a89871ca7e58ec6b8e62a9fafdb5997dd809c6
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2018-11-04 20:17:21 +0200

    JSONTokenizer.skipVoidTokens(): Update Javadoc dummy.

--- project/json-parser/src/main/java/fi/tamk/tiko/ohjelmointi/json/JSONTokenizer.java
+++ project/json-parser/src/main/java/fi/tamk/tiko/ohjelmointi/json/JSONTokenizer.java
@@ -21,6 +21,9 @@ public class JSONTokenizer {
 
     /**
      *
+     *
+     * @param findEOL
+     * @return
      */
     private char skipVoidTokens(boolean findEOL) {
         while (position < input.length()) {
