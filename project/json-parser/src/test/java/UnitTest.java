44ce5388901ca3269acbe7ff9c2b8bc2a8f98244
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2018-11-07 08:34:19 +0200

    UnitTest: Implement JSONObject test.

--- project/json-parser/src/test/java/UnitTest.java
+++ project/json-parser/src/test/java/UnitTest.java
@@ -29,4 +29,32 @@ public class UnitTest {
 
         Assert.assertNull(tokenizer.parseNext());
     }
+
+    /**
+     *
+     */
+    @Test
+    public void testJSONObject() {
+        final String JSON_STRING = "{'key1': 'item1', 'key2': 'item2', 'key3': 'item3'}";
+
+        JSONTokenizer tokenizer = new JSONTokenizer(JSON_STRING);
+        JSONType object = tokenizer.parseNext();
+
+        Assert.assertTrue(object instanceof JSONType);
+        Assert.assertEquals(JSONTypes.OBJECT, object.getType());
+
+        JSONObject array = object.getAsObject();
+        Assert.assertEquals(3, array.size());
+
+        for (int i = 0; i < array.size(); i++) {
+            final String KEY_STRING = String.format("key%d", i + 1);
+            final String ITEM_STRING = String.format("item%d", i + 1);
+
+            Assert.assertTrue(array.containsKey(KEY_STRING));
+            Assert.assertEquals(ITEM_STRING, array.get(KEY_STRING).getAsString());
+            System.out.printf("Success: \"%s\" was found at [\"%s\"]%n", ITEM_STRING, KEY_STRING);
+        }
+
+        Assert.assertNull(tokenizer.parseNext());
+    }
 }
