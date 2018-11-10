fe43cb71bbf902da1dd5a14ba771f91b2e8a34e8
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2018-11-10 17:42:37 +0200

    JSONTypes: Add Javadoc.

--- project/json-parser/src/main/java/fi/tamk/tiko/ohjelmointi/json/JSONTypes.java
+++ project/json-parser/src/main/java/fi/tamk/tiko/ohjelmointi/json/JSONTypes.java
@@ -1,12 +1,46 @@
 package fi.tamk.tiko.ohjelmointi.json;
 
 /**
- *
+ * Defines allowed types for JSON.
  *
  * @author  Joonas Lauhala {@literal <joonas.lauhala@cs.tamk.fi>}
  * @version 2018.1101
  * @since   11
  */
 public enum JSONTypes {
-    BOOLEAN, NUMBER, DECIMAL, STRING, OBJECT, ARRAY, NULL
+
+    /**
+     * Allows boolean values.
+     */
+    BOOLEAN,
+
+    /**
+     * Allows number values.
+     */
+    NUMBER,
+
+    /**
+     * Allows decimal values.
+     */
+    DECIMAL,
+
+    /**
+     * Allows string values.
+     */
+    STRING,
+
+    /**
+     * Allows object values.
+     */
+    OBJECT,
+
+    /**
+     * Allows array values.
+     */
+    ARRAY,
+
+    /**
+     * Allows null values.
+     */
+    NULL
 }
