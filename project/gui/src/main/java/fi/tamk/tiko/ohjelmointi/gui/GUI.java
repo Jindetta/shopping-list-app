311f39098957270b4c9a5b325ade497877312336
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2018-11-17 07:51:08 +0200

    GUI: Implement basic user interface.

--- project/gui/src/main/java/fi/tamk/tiko/ohjelmointi/gui/GUI.java
+++ project/gui/src/main/java/fi/tamk/tiko/ohjelmointi/gui/GUI.java
@@ -5,6 +5,21 @@ import fi.tamk.tiko.ohjelmointi.json.*;
 import javafx.application.Application;
 import javafx.application.Platform;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import javafx.scene.layout.BorderPane;
+
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+
+import javafx.scene.Scene;
+
 import javafx.stage.Stage;
 
 /**
@@ -17,11 +32,74 @@ import javafx.stage.Stage;
 public class GUI extends Application {
 
     /**
+     * 
+     */
+    private ObservableList<Item> items;
+
+    /**
+     * 
+     * @return
+     */
+    private MenuBar createMenuBar() {
+        MenuBar menu = new MenuBar();
+
+        Menu file = new Menu("File");
+        Menu edit = new Menu("Edit");
+        Menu about = new Menu("About");
+
+        menu.getMenus().addAll(file, edit, about);
+
+        return menu;
+    }
+
+    private TableView<Item> createTableView() {
+        TableView<Item> table = new TableView<>();
+
+        TableColumn<Item, String> amount = new TableColumn<>("Amount");
+        amount.setCellValueFactory(new PropertyValueFactory<>("itemAmount"));
+        table.getColumns().add(amount);
+
+        TableColumn<Item, Integer> item = new TableColumn<>("Item");
+        item.setCellValueFactory(new PropertyValueFactory<>("itemName"));
+        table.getColumns().add(item);
+
+        // TODO: Load items or use default view
+        table.setItems(items);
+
+        return table;
+    }
+
+    /**
+     * 
+     */
+    private Scene createSceneContainer() {
+        BorderPane panel = new BorderPane();
+
+        panel.setTop(createMenuBar());
+        panel.setCenter(createTableView());
+        panel.setBottom(new Label("Author: Joonas Lauhala"));
+
+        return new Scene(panel);
+    }
+
+    /**
+     * 
+     */
+    @Override
+    public void init() {
+        items = FXCollections.observableArrayList(
+            new Item(0, "Item 1"),
+            new Item(0, "Item 2")
+        );
+    }
+
+    /**
      * @see javafx.application.Application#start(javafx.stage.Stage) start
      */
     @Override
     public void start(Stage stage) {
         stage.setTitle("Shopping List App");
+        stage.setScene(createSceneContainer());
         stage.setOnCloseRequest(e -> Platform.exit());
         stage.show();
     }
