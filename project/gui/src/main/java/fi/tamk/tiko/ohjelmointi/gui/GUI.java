8e1283ffb13fbe275a67600c431ac7d0fa1824c1
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2018-11-18 17:04:57 +0200

    GUI.createMenuBar(): Add Javadoc.

--- project/gui/src/main/java/fi/tamk/tiko/ohjelmointi/gui/GUI.java
+++ project/gui/src/main/java/fi/tamk/tiko/ohjelmointi/gui/GUI.java
@@ -71,8 +71,8 @@ public class GUI extends Application {
     }
 
     /**
-     * 
-     * @return
+     * Creates menu bar.
+     * @return MenuBar.
      */
     private MenuBar createMenuBar() {
         MenuBar menu = new MenuBar();
