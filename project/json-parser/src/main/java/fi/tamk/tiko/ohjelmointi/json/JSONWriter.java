d735e37f88dc33796b7ee8fa2ecb1c7fd93b9511
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2018-11-20 08:23:47 +0200

    JSONWriter: Complete Javadoc.

--- project/json-parser/src/main/java/fi/tamk/tiko/ohjelmointi/json/JSONWriter.java
+++ project/json-parser/src/main/java/fi/tamk/tiko/ohjelmointi/json/JSONWriter.java
@@ -4,7 +4,7 @@ import java.io.Writer;
 import java.io.IOException;
 
 /**
- *
+ * Writes JSON formatted data.
  *
  * @author  Joonas Lauhala {@literal <joonas.lauhala@cs.tamk.fi>}
  * @version 2018.1101
@@ -13,83 +13,93 @@ import java.io.IOException;
 public class JSONWriter implements AutoCloseable {
 
     /**
-     *
+     * Stores {@link Writer}.
      */
     private Writer writer;
 
     /**
+     * Writes JSON data type.
      *
-     *
-     * @param value
+     * @param value JSONType.
+     * @throws IOException On write operation failure.
      */
     public void write(JSONType value) throws IOException {
         writer.write(value.toString());
     }
 
     /**
-     * 
-     * @param value
-     * @throws IOException
+     * Writes JSON array.
+     *
+     * @param value JSONArray.
+     * @throws IOException On write operation failure.
      */
     public void writeArray(JSONArray value) throws IOException {
         write(JSONType.createArray(value));
     }
 
     /**
-     * 
-     * @param value
-     * @throws IOException
+     * Writes JSON object.
+     *
+     * @param value JSONObject.
+     * @throws IOException On write operation failure.
      */
     public void writeObject(JSONObject value) throws IOException {
         write(JSONType.createObject(value));
     }
 
     /**
-     * 
-     * @param value
-     * @throws IOException
+     * Writes JSON string.
+     *
+     * @param value String.
+     * @throws IOException On write operation failure.
      */
     public void writeString(String value) throws IOException {
         write(JSONType.createString(value));
     }
 
     /**
-     * 
-     * @param value
-     * @throws IOException
+     * Writes JSON decimal.
+     *
+     * @param value Double.
+     * @throws IOException On write operation failure.
      */
     public void writeDecimal(Double value) throws IOException {
         write(JSONType.createDecimal(value));
     }
 
     /**
-     * 
-     * @param value
-     * @throws IOException
+     * Writes JSON number.
+     *
+     * @param value Long.
+     * @throws IOException On write operation failure.
      */
     public void writeNumber(Long value) throws IOException {
         write(JSONType.createNumber(value));
     }
 
     /**
-     * 
-     * @param value
-     * @throws IOException
+     * Writes JSON boolean.
+     *
+     * @param value Boolean.
+     * @throws IOException On write operation failure.
      */
     public void writeBoolean(Boolean value) throws IOException {
         write(JSONType.createBoolean(value));
     }
 
     /**
-     * 
-     * @throws IOException
+     * Writes null.
+     *
+     * @throws IOException On write operation failure.
      */
     public void writeNull() throws IOException {
         write(JSONType.createNull());
     }
 
     /**
+     * Overrides default constructor.
      *
+     * @param writer Writer object.
      */
     public JSONWriter(Writer writer) {
         this.writer = writer;
