55f7a654f1e83ab719ae4d4d30993742dc1c1709
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2018-11-27 19:17:06 +0200

    Tidy import statements.

--- project/gui/src/main/java/fi/tamk/tiko/ohjelmointi/gui/GUI.java
+++ project/gui/src/main/java/fi/tamk/tiko/ohjelmointi/gui/GUI.java
@@ -2,34 +2,36 @@ package fi.tamk.tiko.ohjelmointi.gui;
 
 import fi.tamk.tiko.ohjelmointi.json.*;
 
+import javafx.util.converter.IntegerStringConverter;
+import java.util.stream.Collectors;
+
 import javafx.application.Application;
 import javafx.application.Platform;
+
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.FXML;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.TableView.TableViewSelectionModel;
 import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.TableView.TableViewSelectionModel;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TableColumn;
 import javafx.scene.Scene;
+
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.util.converter.IntegerStringConverter;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.stream.Collectors;
 
 /**
  * Constructs the GUI for Shopping List application.
