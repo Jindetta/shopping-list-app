54a380cf1ab3de6089dcbb519176f614ca885360
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2018-11-27 13:32:20 +0200

    Rename JSONParseTest to JSONEqualityTest.

--- project/json-parser/src/test/java/JSONEqualityTest.java
+++ project/json-parser/src/test/java/JSONEqualityTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.*;
 
 import fi.tamk.tiko.ohjelmointi.json.*;
 
-public class JSONParseTest extends Assertions {
+public class JSONEqualityTest extends Assertions {
 
     /**
      *
