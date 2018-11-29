32dca53916b86a3b0c752f209a99ae4b75eb46c1
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2018-11-29 02:54:41 +0200

    JSONMapper: Change class implementation.

--- project/json-parser/src/main/java/fi/tamk/tiko/ohjelmointi/json/map/JSONMapper.java
+++ project/json-parser/src/main/java/fi/tamk/tiko/ohjelmointi/json/map/JSONMapper.java
@@ -2,7 +2,7 @@ package fi.tamk.tiko.ohjelmointi.json.map;
 
 import fi.tamk.tiko.ohjelmointi.json.*;
 
-import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  *
@@ -16,28 +16,64 @@ public abstract class JSONMapper {
     /**
      * 
      * @param object
-     * @param classData
+     * @param container
      * @return
      */
-    public static <T extends JSONMappable> T map(T object, JSONObject classData) {
+    public static <T extends JSONMappable> T map(T object, JSONObject container) {
         try {
-            if (!object.isJSONMappable()) {
-                throw new IllegalStateException();
-            }
-
             Class<?> classInfo = object.getClass();
+            JSONObject data = getObjectData(object, container);
 
-            for (String key : classData.keySet()) {
-                JSONType jsonType = classData.get(key);
-                Field field = classInfo.getDeclaredField(key);
+            for (String key : data.keySet()) {
+                Object value = data.get(key).get();
+                Class<?> type = value == null ? Object.class : value.getClass();
+                Method setter = classInfo.getMethod(getSetter(key), type);
 
-                field.trySetAccessible();
-                field.set(object, jsonType.get());
+                setter.trySetAccessible();
+                setter.invoke(object, value);
             }
         } catch (Exception e) {
             throw new IllegalStateException("JSONMapper cannot map this object.");
         }
 
         return object;
+    }
+
+    /**
+     * 
+     * @param classType
+     * @param object
+     * @return
+     */
+    public static JSONObject setContainer(Class<? extends JSONMappable> classType, JSONObject object) {
+        JSONObject container = new JSONObject();
+
+        container.putObject(classType.getName(), object);
+
+        return container;
+    }
+
+    /**
+     * 
+     * @param object
+     * @param className
+     * @param data
+     * @return
+     */
+    private static <T extends JSONMappable> JSONObject getObjectData(T object, JSONObject data) {
+        if (!object.isJSONMappable()) {
+            throw new IllegalStateException();
+        }
+
+        return data.get(object.getClass().getName()).getAsObject();
+    }
+
+    /**
+     * 
+     * @param name
+     * @return
+     */
+    private static String getSetter(String name) {
+        return "set".concat(name.substring(0, 1).toUpperCase().concat(name.substring(1)));
     }
 }
