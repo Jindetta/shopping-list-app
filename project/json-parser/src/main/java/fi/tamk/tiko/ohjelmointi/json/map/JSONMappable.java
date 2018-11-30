43fa7f0482879e8e0ac3eb0fb0ecb9ceecfb3f82
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2018-11-30 08:22:24 +0200

    JSONMappable: Change from interface to annotation.

--- project/json-parser/src/main/java/fi/tamk/tiko/ohjelmointi/json/map/JSONMappable.java
+++ project/json-parser/src/main/java/fi/tamk/tiko/ohjelmointi/json/map/JSONMappable.java
@@ -1,12 +1,17 @@
 package fi.tamk.tiko.ohjelmointi.json.map;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
 /**
- *
+ * Defines annotation to be used on JSON Mappable classes.
  *
  * @author  Joonas Lauhala {@literal <joonas.lauhala@cs.tamk.fi>}
  * @version 2018.1101
  * @since   11
  */
-public interface JSONMappable {
-    boolean isJSONMappable();
-}
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface JSONMappable {}
