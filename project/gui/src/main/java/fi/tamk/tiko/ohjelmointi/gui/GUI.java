51a657039deabc39ccb48b99ed1e98b09945ed1a
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2018-11-30 09:02:16 +0200

    Add imports for Clipboard and ClipboardContent.

--- project/gui/src/main/java/fi/tamk/tiko/ohjelmointi/gui/GUI.java
+++ project/gui/src/main/java/fi/tamk/tiko/ohjelmointi/gui/GUI.java
@@ -20,6 +20,7 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.Alert;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableView;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextInputDialog;
@@ -27,13 +28,14 @@ import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.control.TableView.TableViewSelectionModel;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.control.ButtonType;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Clipboard;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.Scene;
 
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileReader;
