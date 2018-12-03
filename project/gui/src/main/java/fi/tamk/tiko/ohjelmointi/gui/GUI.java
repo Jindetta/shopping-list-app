09ef37f4bdc76c7db52aecbf38f9eb51b0d53c05
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2018-12-04 00:30:26 +0200

    GUI: Implement FXML events/controls.

--- project/gui/src/main/java/fi/tamk/tiko/ohjelmointi/gui/GUI.java
+++ project/gui/src/main/java/fi/tamk/tiko/ohjelmointi/gui/GUI.java
@@ -13,6 +13,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
@@ -86,6 +87,12 @@ public class GUI extends Application {
      */
     @FXML
     private TableColumn<Item, String> columnItem;
+
+    /**
+     * 
+     */
+    @FXML
+    private CheckBox toggleEditMode;
 
     /**
      * 
@@ -287,8 +294,18 @@ public class GUI extends Application {
 
         items.add(selected, new Item());
 
-        tableView.layout();
-        tableView.edit(selected, columnAmount);
+        if (toggleEditMode.isSelected()) {
+            tableView.layout();
+            tableView.edit(selected, columnAmount);
+        }
+    }
+
+    /**
+     * 
+     */
+    @FXML
+    private void onEditItemAction() {
+        tableView.edit(selection.getSelectedIndex(), columnAmount);
     }
 
     /**
