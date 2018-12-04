95e14401bb5ff75ebe2b033aa42cc81cf813632c
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2018-12-04 14:08:58 +0200

    CustomLongConverter: Complete Javadoc.

--- project/gui/src/main/java/fi/tamk/tiko/ohjelmointi/gui/CustomLongConverter.java
+++ project/gui/src/main/java/fi/tamk/tiko/ohjelmointi/gui/CustomLongConverter.java
@@ -2,15 +2,22 @@ package fi.tamk.tiko.ohjelmointi.gui;
 
 import javafx.util.converter.LongStringConverter;
 
+/**
+ * Custom version of LongStringConverter.
+ *
+ * @author  Joonas Lauhala {@literal <joonas.lauhala@cs.tamk.fi>}
+ * @version 2018.1101
+ * @since   11
+ */
 public class CustomLongConverter extends LongStringConverter {
 
     /**
-     * 
+     * Stores value.
      */
     private Long value;
 
     /**
-     * 
+     * Overrides method to handle exceptions.
      */
     @Override
     public Long fromString(String value) {
