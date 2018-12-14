d56a968aedf351312364735b35d3337b83932666
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2018-12-14 08:43:38 +0200

    GUI.onSaveAsAction(): Set initial directory to saveFile directory.

--- project/gui/src/main/java/fi/tamk/tiko/ohjelmointi/gui/GUI.java
+++ project/gui/src/main/java/fi/tamk/tiko/ohjelmointi/gui/GUI.java
@@ -152,7 +152,7 @@ public class GUI extends Application {
 
         fileChooser.setTitle("Save shopping list data");
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JSON Files", "*.json"));
-        fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
+        fileChooser.setInitialDirectory(new File(getFileDirectoryPath(saveFile)));
         fileChooser.setInitialFileName(saveFile.getName());
 
         File file = fileChooser.showSaveDialog(window);
